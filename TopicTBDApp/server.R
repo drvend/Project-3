ce9de74c2e3ae84783bda74c1b0e8fd87de11da0
drvend <drvendit@ncsu.edu>
2020-11-13 08:33:22 -0500

    Troubleshooted bugs in office hours

--- TopicTBDApp/server.R
+++ TopicTBDApp/server.R
@@ -6,74 +6,86 @@
 #
 #    http://shiny.rstudio.com/
 #
-
+# packages required to run this coded are listed below
 library(shiny)
 library(readr)
 library(dplyr)
 library(reshape2)
 library(ggplot2)
+library(caret)
+#library(DT)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
+    # 
+    # FFdataw1 <- read.csv('https://raw.githubusercontent.com/fantasydatapros/data/master/weekly/2019/week1.csv', header= TRUE, stringsAsFactors = TRUE)
+    # FFdataw1 <- mutate(FFdataw1, Week = 1)
+    # 
+    # FFdataw2 <- read.csv('https://raw.githubusercontent.com/fantasydatapros/data/master/weekly/2019/week2.csv', header= TRUE, stringsAsFactors = TRUE)
+    # FFdataw2 <- mutate(FFdataw2, Week = 2)
+    # 
+    # FFdataw3 <- read.csv('https://raw.githubusercontent.com/fantasydatapros/data/master/weekly/2019/week3.csv', header= TRUE, stringsAsFactors = TRUE)
+    # FFdataw3 <- mutate(FFdataw3, Week = 3)
+    # 
+    # FFdataw4 <- read.csv('https://raw.githubusercontent.com/fantasydatapros/data/master/weekly/2019/week4.csv', header= TRUE, stringsAsFactors = TRUE)
+    # FFdataw4 <- mutate(FFdataw4, Week = 4)
+    # 
+    # FFdataw5 <- read.csv('https://raw.githubusercontent.com/fantasydatapros/data/master/weekly/2019/week5.csv', header= TRUE, stringsAsFactors = TRUE)
+    # FFdataw5 <- mutate(FFdataw5, Week = 5)
+    # 
+    # FFdataw6 <- read.csv('https://raw.githubusercontent.com/fantasydatapros/data/master/weekly/2019/week6.csv', header= TRUE, stringsAsFactors = TRUE)
+    # FFdataw6 <- mutate(FFdataw6, Week = 6)
+    # 
+    # FFdataw7 <- read.csv('https://raw.githubusercontent.com/fantasydatapros/data/master/weekly/2019/week7.csv', header= TRUE, stringsAsFactors = TRUE)
+    # FFdataw7 <- mutate(FFdataw7, Week = 7)
+    # 
+    # FFdataw8 <- read.csv('https://raw.githubusercontent.com/fantasydatapros/data/master/weekly/2019/week8.csv', header= TRUE, stringsAsFactors = TRUE)
+    # FFdataw8 <- mutate(FFdataw8, Week = 8)
+    # 
+    # FFdataw9 <- read.csv('https://raw.githubusercontent.com/fantasydatapros/data/master/weekly/2019/week9.csv', header= TRUE, stringsAsFactors = TRUE)
+    # FFdataw9 <- mutate(FFdataw9, Week = 9)
+    # 
+    # FFdataw10 <- read.csv('https://raw.githubusercontent.com/fantasydatapros/data/master/weekly/2019/week10.csv', header= TRUE, stringsAsFactors = TRUE)
+    # FFdataw10 <- mutate(FFdataw10, Week = 10)
+    # 
+    # FFdataw11 <- read.csv('https://raw.githubusercontent.com/fantasydatapros/data/master/weekly/2019/week11.csv', header= TRUE, stringsAsFactors = TRUE)
+    # FFdataw11 <- mutate(FFdataw11, Week = 11)
+    # 
+    # FFdataw12 <- read.csv('https://raw.githubusercontent.com/fantasydatapros/data/master/weekly/2019/week12.csv', header= TRUE, stringsAsFactors = TRUE)
+    # FFdataw12 <- mutate(FFdataw12, Week = 12)
+    # 
+    # FFdataw13 <- read.csv('https://raw.githubusercontent.com/fantasydatapros/data/master/weekly/2019/week13.csv', header= TRUE, stringsAsFactors = TRUE)
+    # FFdataw13 <- mutate(FFdataw13, Week = 13)
+    # 
+    # FFdataw14 <- read.csv('https://raw.githubusercontent.com/fantasydatapros/data/master/weekly/2019/week14.csv', header= TRUE, stringsAsFactors = TRUE)
+    # FFdataw14 <- mutate(FFdataw14, Week = 14)
+    # 
+    # FFdataw15 <- read.csv('https://raw.githubusercontent.com/fantasydatapros/data/master/weekly/2019/week15.csv', header= TRUE, stringsAsFactors = TRUE)
+    # FFdataw15 <- mutate(FFdataw15, Week = 15)
+    # 
+    # FFdataw16 <- read.csv('https://raw.githubusercontent.com/fantasydatapros/data/master/weekly/2019/week16.csv', header= TRUE, stringsAsFactors = TRUE)
+    # FFdataw16 <- mutate(FFdataw16, Week = 16)
+    # 
+    # FFdataw17 <- read.csv('https://raw.githubusercontent.com/fantasydatapros/data/master/weekly/2019/week17.csv', header= TRUE, stringsAsFactors = TRUE)
+    # FFdataw17 <- mutate(FFdataw17, Week = 17)
+    # 
+    # Predictiondata <- read.csv('C:/Users/drven/Documents/ST 558/Project-3/2019projections.csv')
+    # 
+    # FFdatatotal <- rbind(FFdataw1, FFdataw2, FFdataw3, FFdataw4, FFdataw5, FFdataw6, FFdataw7, FFdataw8, FFdataw9, FFdataw10, FFdataw11, FFdataw12, FFdataw13, FFdataw14, FFdataw15, FFdataw16, FFdataw17)
+    # 
+    # saveRDS(FFdatatotal, "FFdatatotal.RDS")
     
-    FFdataw1 <- read.csv('https://raw.githubusercontent.com/fantasydatapros/data/master/weekly/2019/week1.csv', header= TRUE, stringsAsFactors = TRUE)
-    FFdataw1 <- mutate(FFdataw1, Week = 1)
     
-    FFdataw2 <- read.csv('https://raw.githubusercontent.com/fantasydatapros/data/master/weekly/2019/week2.csv', header= TRUE, stringsAsFactors = TRUE)
-    FFdataw2 <- mutate(FFdataw1, Week = 2)
     
-    FFdataw3 <- read.csv('https://raw.githubusercontent.com/fantasydatapros/data/master/weekly/2019/week3.csv', header= TRUE, stringsAsFactors = TRUE)
-    FFdataw3 <- mutate(FFdataw1, Week = 3)
+    ########
     
-    FFdataw4 <- read.csv('https://raw.githubusercontent.com/fantasydatapros/data/master/weekly/2019/week4.csv', header= TRUE, stringsAsFactors = TRUE)
-    FFdataw4 <- mutate(FFdataw1, Week = 4)
     
-    FFdataw5 <- read.csv('https://raw.githubusercontent.com/fantasydatapros/data/master/weekly/2019/week5.csv', header= TRUE, stringsAsFactors = TRUE)
-    FFdataw5 <- mutate(FFdataw1, Week = 5)
+    FFdatatotal = readRDS("C:/Users/drven/Documents/ST 558/Project-3/FFdatatotal.RDS")
     
-    FFdataw6 <- read.csv('https://raw.githubusercontent.com/fantasydatapros/data/master/weekly/2019/week6.csv', header= TRUE, stringsAsFactors = TRUE)
-    FFdataw6 <- mutate(FFdataw1, Week = 6)
     
-    FFdataw7 <- read.csv('https://raw.githubusercontent.com/fantasydatapros/data/master/weekly/2019/week7.csv', header= TRUE, stringsAsFactors = TRUE)
-    FFdataw7 <- mutate(FFdataw1, Week = 7)
     
-    FFdataw8 <- read.csv('https://raw.githubusercontent.com/fantasydatapros/data/master/weekly/2019/week8.csv', header= TRUE, stringsAsFactors = TRUE)
-    FFdataw8 <- mutate(FFdataw1, Week = 8)
-    
-    FFdataw9 <- read.csv('https://raw.githubusercontent.com/fantasydatapros/data/master/weekly/2019/week9.csv', header= TRUE, stringsAsFactors = TRUE)
-    FFdataw9 <- mutate(FFdataw1, Week = 9)
-    
-    FFdataw10 <- read.csv('https://raw.githubusercontent.com/fantasydatapros/data/master/weekly/2019/week10.csv', header= TRUE, stringsAsFactors = TRUE)
-    FFdataw10 <- mutate(FFdataw1, Week = 10)
-    
-    FFdataw11 <- read.csv('https://raw.githubusercontent.com/fantasydatapros/data/master/weekly/2019/week11.csv', header= TRUE, stringsAsFactors = TRUE)
-    FFdataw11 <- mutate(FFdataw1, Week = 11)
-    
-    FFdataw12 <- read.csv('https://raw.githubusercontent.com/fantasydatapros/data/master/weekly/2019/week12.csv', header= TRUE, stringsAsFactors = TRUE)
-    FFdataw12 <- mutate(FFdataw1, Week = 12)
-    
-    FFdataw13 <- read.csv('https://raw.githubusercontent.com/fantasydatapros/data/master/weekly/2019/week13.csv', header= TRUE, stringsAsFactors = TRUE)
-    FFdataw13 <- mutate(FFdataw1, Week = 13)
-    
-    FFdataw14 <- read.csv('https://raw.githubusercontent.com/fantasydatapros/data/master/weekly/2019/week14.csv', header= TRUE, stringsAsFactors = TRUE)
-    FFdataw14 <- mutate(FFdataw1, Week = 14)
-    
-    FFdataw15 <- read.csv('https://raw.githubusercontent.com/fantasydatapros/data/master/weekly/2019/week15.csv', header= TRUE, stringsAsFactors = TRUE)
-    FFdataw15 <- mutate(FFdataw1, Week = 15)
-    
-    FFdataw16 <- read.csv('https://raw.githubusercontent.com/fantasydatapros/data/master/weekly/2019/week16.csv', header= TRUE, stringsAsFactors = TRUE)
-    FFdataw16 <- mutate(FFdataw1, Week = 16)
-    
-    FFdataw17 <- read.csv('https://raw.githubusercontent.com/fantasydatapros/data/master/weekly/2019/week17.csv', header= TRUE, stringsAsFactors = TRUE)
-    FFdataw17 <- mutate(FFdataw1, Week = 17)
-    
-    Predictiondata <- read.csv('C:/Users/drven/Documents/ST 558/Project-3/2019projections.csv')
-    
-    FFdatatotal <- rbind(FFdataw1, FFdataw2, FFdataw3, FFdataw4, FFdataw5, FFdataw6, FFdataw7, FFdataw8, FFdataw9, FFdataw10, FFdataw11, FFdataw12, FFdataw13, FFdataw14, FFdataw15, FFdataw16, FFdataw17)
     
     fantasyData <- left_join(Predictiondata, FFdatatotal, by = c("Player", "Week"))
-
-    shinyServer(function(input, output, session) {
 
         # Create Correlation heatmap
         
@@ -150,6 +162,7 @@ melted_cor <- filter(melted_cor, Var1 %in% c("StandardFantasyPoints", "PPRFantas
             theme(axis.text.x = element_text(angle = 90, vjust = 1, 
                                              size = 10, hjust = 1))+
             coord_fixed()
+        
         # Print the heatmap
         print(ggheatmap)
         
@@ -169,6 +182,24 @@ melted_cor <- filter(melted_cor, Var1 %in% c("StandardFantasyPoints", "PPRFantas
         print(ggheatmap)
 
         output$heatmap <- renderPlot({ggheatmap})
-    })
-
+        
+        output$summary <- renderTable({fantasyData %>% group_by(Player) %>% summarise(avgProj = mean(Proj), avgActual = mean(Actual), avgPA = mean(PassingAtt), avgRA = mean(RushingAtt), avgTgt = mean(Tgt))
+        })
+    
+# Clustering with Dendogram 
+        
+  #  scaledData <- scale(fantasyDataNumeric, center = TRUE, scale = TRUE)
+        
+#clustmodel <- hclust()
+        
+        
+# Modeling 
+        
+        
+    # Data Table for the Data Table tab 
+    output$rawData <- renderDataTable({fantasyData}, 
+                    extensions = 'Buttons',
+                    options = list("dom" = 'T<"clear">lBfrtip', buttons = list('copy', 'csv', 'excel', 'pdf', 'print'))
+    )
+    
 })
--- TopicTBDApp/ui.R
+++ TopicTBDApp/ui.R
@@ -18,25 +18,26 @@ shinyUI(fluidPage(
   tabsetPanel(
     tabPanel("Information Page", 
              sidebarLayout(
-               sidebarPanel("You can use the sidebar panel on other tabs to select variables and toggle between different variables, graphs and analysis options."), 
+               sidebarPanel("On other tabs you can use widgets on the sidebar to select variables and toggle between different variables, graphs and analysis options."), 
         mainPanel(h4("This data is from multiple data sets, all provided by"),
         h4(strong(("Fantasy Football Data Pros."))), h4("The links to download or access the data are here:"), a("Fantasy Football Pros Website", href="https://www.fantasyfootballdatapros.com/csv_files"), br(), a("Fantasy Football Pros Github Repository", href="https://github.com/fantasydatapros/data/blob/master/README.md"), br(),br(), "This data contains information on NFL football players and their Fantasy Football Scoring as well as ancillary variables for the 2019 season.", br(), br(), "You may navigate the app using the tabs above which contain exploratory data analysis, Clustering Analysis, Predictive Modeling, and a tab from which you can view and download the raw data."
         )
     )
 ),
 
-tabPanel("DataExploration",
+tabPanel("Data Exploration",
          sidebarLayout(
              sidebarPanel(),
              mainPanel(
-                 plotOutput("ggheatmap"),)
+               plotOutput("heatmap"), 
+               tableOutput("summary"))
          )
          ),
 
-tabPanel("Clustering or Principal Components Analysis", ),
+tabPanel("Clustering Analysis", ),
 
 tabPanel("Modeling",),
 
-tabPanel("Data Table",)
+tabPanel("Data Table", dataTableOutput("rawData"))
 )
 ))
